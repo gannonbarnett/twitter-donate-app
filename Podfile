1ced8e9ccecbf30af72f628d50e093aea1179080
gbarnett <gannonbarnett@gannons-macbook-air.local>
2018-03-26 12:46:53 -0400

    Cocoapods added: Firebase, FirebaseDatabase, TwitterKit

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '9.0'
+
+target 'TwonateApplication' do
+
+	pod 'Firebase'
+	pod 'Firebase/Database'
+	pod 'TwitterKit' 
+
+	use_frameworks!
+
+end
---
+++ TwonateApplication/TwitterFunctions.swift
@@ -0,0 +1,9 @@
+//
+//  TwitterFunctions.swift
+//  TwonateApplication
+//
+//  Created by Gannon Barnett on 3/26/18.
+//  Copyright © 2018 BarnettDevelopmentCompany. All rights reserved.
+//
+
+import Foundation
